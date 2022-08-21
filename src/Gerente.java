f3d898dec75ff3029b02289cb2da8264e35d42a4
tbadarodeoliveira <tbadarodeoliveira@gmail.com>
2022-08-21 10:23:32 -0300

    Iniciando com interfaces

--- src/Gerente.java
+++ src/Gerente.java
@@ -1,20 +1,7 @@
 
-public class Gerente extends Funcionario{
-
-	private int senha;
+public class Gerente extends Funcionario {
 	
-	public void setSenha(int senha) {
-		this.senha = senha;
-	}
-	
-	public boolean autentica(int senha) {
-		if(this.senha == senha) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
+	@Override
 	public double getBonificacao() {
 		return 200;
 	}	
--- src/TesteGerente.java
+++ src/TesteGerente.java
@@ -6,15 +6,15 @@ public class TesteGerente {
 		g1.setNome("Marco");
 		g1.setCpf("223342513");
 		g1.setSalario(5000.0);
-		g1.setSenha(2222);
+		//g1.setSenha(2222);
 		
 		System.out.println(g1.getNome());
 		System.out.println(g1.getCpf());
 		System.out.println(g1.getSalario());
 		
-		boolean autenticou = g1.autentica(2222);
+		//boolean autenticou = g1.autentica(2222);
 		
-		System.out.println(autenticou);
+		//System.out.println(autenticou);
 		
 		
 
