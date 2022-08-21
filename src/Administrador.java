9371fa433af3ffa04674a345872737b9ba10fd0b
tbadarodeoliveira <tbadarodeoliveira@gmail.com>
2022-08-21 11:32:01 -0300

    Composicao

--- src/Administrador.java
+++ src/Administrador.java
@@ -1,9 +1,26 @@
 
-public class Administrador extends Funcionario {
+public class Administrador extends Funcionario implements Autenticavel {
 
+	private AutenticacaoUtil autenticador;
+	
 	@Override
 	public double getBonificacao() {
 		return 110;
 	}
+	
+	public Administrador() {
+		this.autenticador = new AutenticacaoUtil();
+	}
+
+	@Override
+	public void setSenha(int senha) {
+		this.autenticador.setSenha(senha);		
+	}
+
+	@Override
+	public boolean autentica(int senha) {
+		return this.autenticador.autentica(senha);
+	}
+		
 
 }
--- src/Gerente.java
+++ src/Gerente.java
@@ -1,9 +1,24 @@
-
-public class Gerente extends Funcionario {
+public class Gerente extends Funcionario implements Autenticavel{
+	
+	private AutenticacaoUtil autenticador;
 	
 	@Override
 	public double getBonificacao() {
 		return 200;
-	}	
+	}
+
+	public Gerente() {
+		this.autenticador = new AutenticacaoUtil();
+	}
+
+	@Override
+	public void setSenha(int senha) {
+		this.autenticador.setSenha(senha);		
+	}
+
+	@Override
+	public boolean autentica(int senha) {
+		return this.autenticador.autentica(senha);
+	}
 		
 }
